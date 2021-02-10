6a53898ee2b39654dd752c5359eb7413e9e54587
Nawaraj Shah <shah.nawaraj.ns@gmail.com>
2021-02-10 15:54:24 +0545

    Update forgot_password_process.php

--- forgot_password_process.php
+++ forgot_password_process.php
@@ -22,13 +22,13 @@
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'nawarajshah129@gmail.com';                     // SMTP username
-        $mail->Password   = 'Chemistry2076';                               // SMTP password
+        $mail->Username   = 'your_email@gmail.com';                     // SMTP username
+        $mail->Password   = 'secret';                               // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
     
         //Recipients
-        $mail->setFrom('nawarajshah129@gmail.com', 'Admin');
+        $mail->setFrom('your_email@gmail.com', 'Admin');
         $mail->addAddress($email);     // Add a recipient
 
         $code = substr(str_shuffle('1234567890QWERTYUIOPASDFGHJKLZXCVBNM'),0,10);
@@ -38,7 +38,7 @@
         $mail->Subject = 'Password Reset';
         $mail->Body    = 'To reset your password click <a href="http://localhost/login_system/change_password.php?code='.$code.'">here </a>. </br>Reset your password in a day.';
 
-        $conn = new mySqli('localhost', 'root', '', 'test');
+        $conn = new mySqli('localhost', 'root', '', 'database_username');
 
         if($conn->connect_error) {
             die('Could not connect to the database.');
